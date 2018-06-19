26f77f1d31a61e85ff31889234bd7ae3fdf59075
Mariana Marasoiu <mariana.marasoiu@gmail.com>
2018-06-19 15:38:09 +0100

    Change sample visualisation to bar chart with whiskers (#29)

--- lib/playground.dart
+++ lib/playground.dart
@@ -25,8 +25,12 @@ void setup() {
   {
     List<List<viewmodel.CellViewModel>> spreadsheetCells = viewmodel.sheetbooks[1].selectedSheet.cells;
     spreadsheetCells[0][2].commitFormulaString('Average stock');
+    spreadsheetCells[0][3].commitFormulaString('Max stock');
+    spreadsheetCells[0][4].commitFormulaString('Min stock');
     for (int row = 0; row < otherData.length; row++) {
       spreadsheetCells[row+1][2].commitFormulaString('=Average(Sheet1!B${row+2}:Sheet1!D${row+2})');
+      spreadsheetCells[row+1][3].commitFormulaString('=Max(Sheet1!B${row+2}:Sheet1!D${row+2})');
+      spreadsheetCells[row+1][4].commitFormulaString('=Min(Sheet1!B${row+2}:Sheet1!D${row+2})');
     }
   }
 
@@ -37,67 +41,21 @@ void setup() {
 
     layer.addShape(0, {
       viewmodel.Rect.x: 500,
-      viewmodel.Rect.y: 50,
-      viewmodel.Rect.width: 0,
-      viewmodel.Rect.height: 10});
+      viewmodel.Rect.y: 100,
+      viewmodel.Rect.width: 30,
+      viewmodel.Rect.height: 100});
     sheet.updateRow(0);
-    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.width])].commitFormulaString('=Sheet1!B2 * 5');
-    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.y])].commitFormulaString('=Sheet2!A2 * 50');
+    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.height])].commitFormulaString('=Sheet2!C2 * 5');
+    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('=500 + Sheet2!A2 * 50');
+    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.y])].commitFormulaString('=300 - Height1');
     sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.fillColor])].commitFormulaString('#efc45f');
 
-    sheet.cells[1][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-    sheet.cells[2][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-    sheet.cells[3][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-    sheet.cells[4][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-    sheet.cells[5][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-    sheet.cells[6][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-  }
-
-  {
-    viewmodel.LayerSheetFactory layerSheetFactory = new viewmodel.LayerSheetFactory(viewmodel.Shape.rect);
-    viewmodel.GraphicsSheetViewModel sheet = layerSheetFactory.sheet;
-    viewmodel.LayerViewModel layer = layerSheetFactory.layer;
-
-    layer.addShape(0, {
-      viewmodel.Rect.x: 500,
-      viewmodel.Rect.y: 60,
-      viewmodel.Rect.width: 0,
-      viewmodel.Rect.height: 10});
-    sheet.updateRow(0);
-    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.width])].commitFormulaString('=Sheet1!C2 * 5');
-    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.y])].commitFormulaString('=10 + Sheet2!A2 * 50');
-    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.fillColor])].commitFormulaString('#a675ef');
-
-    sheet.cells[1][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-    sheet.cells[2][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-    sheet.cells[3][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-    sheet.cells[4][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-    sheet.cells[5][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-    sheet.cells[6][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-  }
-
-
-  {
-    viewmodel.LayerSheetFactory layerSheetFactory = new viewmodel.LayerSheetFactory(viewmodel.Shape.rect);
-    viewmodel.GraphicsSheetViewModel sheet = layerSheetFactory.sheet;
-    viewmodel.LayerViewModel layer = layerSheetFactory.layer;
-
-    layer.addShape(0, {
-      viewmodel.Rect.x: 500,
-      viewmodel.Rect.y: 70,
-      viewmodel.Rect.width: 0,
-      viewmodel.Rect.height: 10});
-    sheet.updateRow(0);
-    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.width])].commitFormulaString('=Sheet1!D2 * 5');
-    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.y])].commitFormulaString('=20 + Sheet2!A2 * 50');
-    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.fillColor])].commitFormulaString('#75c4ef');
-
-    sheet.cells[1][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-    sheet.cells[2][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-    sheet.cells[3][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-    sheet.cells[4][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-    sheet.cells[5][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
-    sheet.cells[6][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.x])].commitFormulaString('500');
+    sheet.cells[1][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.width])].commitFormulaString('30');
+    sheet.cells[2][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.width])].commitFormulaString('30');
+    sheet.cells[3][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.width])].commitFormulaString('30');
+    sheet.cells[4][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.width])].commitFormulaString('30');
+    sheet.cells[5][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.width])].commitFormulaString('30');
+    sheet.cells[6][sheet.activeColumnNames.indexOf(viewmodel.rectPropertyToColumnName[viewmodel.Rect.width])].commitFormulaString('30');
   }
 
   {
@@ -111,17 +69,12 @@ void setup() {
       viewmodel.Line.x2: 700,
       viewmodel.Line.y2: 115});
     sheet.updateRow(0);
-    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.linePropertyToColumnName[viewmodel.Line.x1])].commitFormulaString('=500 + Sheet2!C2 * 5');
-    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.linePropertyToColumnName[viewmodel.Line.x2])].commitFormulaString('=500 + Sheet2!C3 * 5');
-    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.linePropertyToColumnName[viewmodel.Line.y1])].commitFormulaString('=15 + Sheet2!A2 * 50');
-    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.linePropertyToColumnName[viewmodel.Line.y2])].commitFormulaString('=15 + Sheet2!A3 * 50');
-    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.linePropertyToColumnName[viewmodel.Line.strokeWidth])].commitFormulaString('2');
 
-    sheet.cells[1][sheet.activeColumnNames.indexOf(viewmodel.linePropertyToColumnName[viewmodel.Line.strokeOpacity])].commitFormulaString('1');
-    sheet.cells[2][sheet.activeColumnNames.indexOf(viewmodel.linePropertyToColumnName[viewmodel.Line.strokeOpacity])].commitFormulaString('1');
-    sheet.cells[3][sheet.activeColumnNames.indexOf(viewmodel.linePropertyToColumnName[viewmodel.Line.strokeOpacity])].commitFormulaString('1');
-    sheet.cells[4][sheet.activeColumnNames.indexOf(viewmodel.linePropertyToColumnName[viewmodel.Line.strokeOpacity])].commitFormulaString('1');
-    sheet.cells[5][sheet.activeColumnNames.indexOf(viewmodel.linePropertyToColumnName[viewmodel.Line.strokeOpacity])].commitFormulaString('1');
+    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.linePropertyToColumnName[viewmodel.Line.x1])].commitFormulaString('=Rect3!X1 + Rect3!Width1 / 2');
+    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.linePropertyToColumnName[viewmodel.Line.x2])].commitFormulaString('=StartX1');
+    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.linePropertyToColumnName[viewmodel.Line.y1])].commitFormulaString('=300 - Sheet2!D2 * 5');
+    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.linePropertyToColumnName[viewmodel.Line.y2])].commitFormulaString('=300 - Sheet2!E2 * 5');
+    sheet.cells[0][sheet.activeColumnNames.indexOf(viewmodel.linePropertyToColumnName[viewmodel.Line.strokeWidth])].commitFormulaString('2');
   }
 }
 
